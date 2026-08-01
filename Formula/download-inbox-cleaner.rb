2dd3c700384120afe8794f43ac28ed2ae538d54e
391 Sutthiphod Roopsom <sutipojr@gmail.com>
2026-08-01 14:41:51 +0700

    Update download-inbox-cleaner.rb

--- Formula/download-inbox-cleaner.rb
+++ Formula/download-inbox-cleaner.rb
@@ -1,1 +1,19 @@
+class DownloadInboxCleaner < Formula
+    desc "Cautious macOS command-line tool for reviewing and organizing Downloads"
+    homepage "https://github.com/stawan15/Download-scanner"
+    version "1.0.0"
 
+    on_arm do
+      url
+      "https://github.com/stawan15/Download-scanner/releases/download/v1.0.0/Download-scanner_1.0.0_darwin_arm64.tar.gz"
+      sha256 "85ccc3825a3d78b9723db05971d7a2f26f5b20e464d1250b2a54135600740c09"
+
+      def install
+        bin.install "cleaner"
+      end
+    end
+
+    test do
+      system "#{bin}/cleaner", "version"
+    end
+  end
